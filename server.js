3ab9a3b698dd320f80115c6a2e64b76126c01ac4
Daeky <91830825+Daeky03@users.noreply.github.com>
2023-11-15 10:36:14 +0300

    Update
    proxy api added

---
+++ server.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.get('/request', async (req, res) => {
+const url = req.query.link;
+if(!url) return;
+  try {
+    const response = await fetch(url);
+    
+    // Diğer rota
+
+const html = await response.text()
+    // API yanıtını kontrol etme
+    if (response.status === 200) {
+      res.status(response.status).send(html);
+    } else {
+      res.status(response.status).send(html);
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
