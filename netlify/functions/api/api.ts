0d25ae3029a7850d7426de90fafcdacb5189d3d0
Daeky <91830825+Daeky03@users.noreply.github.com>
2023-11-16 15:07:41 +0300

    Update api.ts

--- netlify/functions/api/api.ts
+++ netlify/functions/api/api.ts
@@ -1,1 +1,20 @@
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+api.get("/request", (req, res) => {
+  const url = req.query.link
+  if(!url) return;
+  fetch(url).then(response => {
+    const html = await response.text()
+    res.status(response.status).send(html)
+  })
+})
+
+export const handler = serverless(api);
